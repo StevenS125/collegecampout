23df0e88f12692b20bf8acf6006674f4affa608d
makhno <makhno@makhnos-MacBook-Pro.local>
2017-03-26 18:55:45 -0400

    userscript

--- public/js/userScript.js
+++ public/js/userScript.js
@@ -16,3 +16,26 @@ $("#create").on("click", function(event) {
 
 
 });
+
+
+// make sure you 'npm install crypto --save'
+const crypto = require('crypto')
+
+const sha512 = (password, salt) => {
+   let hash = crypto.createHmac('sha512', salt);
+   hash.update(password);
+   let value = hash.digest('hex');
+   return {
+       salt: salt,
+       passwordHash: value
+   }
+};
+
+const genRandomString = (length) => {
+   return crypto.randomBytes(Math.ceil(length/2))
+       .toString('hex')
+       .slice(0,length);
+};
+
+salt = genRandomString(32);
+hashedPassword = sha512(password, salt).passwordHash;
